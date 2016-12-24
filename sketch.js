5187c9a3db7a26cce270f23e9e2777554cd0ddff
Kiraub <kiraub.1997@gmail.com>
2016-12-24 01:44:19 +0100

    Added p5.dom.js as ressource for usage sketch:   added options for the bursh including pencil and eraser mode as well as a slider for the brush size

--- sketch.js
+++ sketch.js
@@ -1,39 +1,71 @@
-var black;
 var x;
 var y;
 var lastx;
 var lasty;
 
+var brushOption;
+var brushSize;
+
+var black = 0;
+
+var backgroundColor = 200;
+var canvasHeight = 600;
+var canvasWidth = 600;
+
+var fps = 60;
+
 function setup()
 {
-    frameRate(60);
-    black = color(0);
-    createCanvas( 600, 600);
-    background(200);
     lastx = -1;
     lasty = -1;
+
+    frameRate( fps);
+    createCanvas( canvasWidth, canvasHeight);
+    background( backgroundColor);
+    
+    brushOption = createRadio();
+    brushOption.option( "pencil");
+    brushOption.option( "eraser");
+    brushOption.style( 'width', '80px');
+
+    brushSize = createSlider( 1, 20, 2, 1);
+
 }
 
 function draw()
 {
-    if( mouseIsPressed )
+    if( mouseIsPressed)
     {
-        x = mouseX;
-        y = mouseY;
-        if( lastx == -1)
-        {
-            lastx = x;
-            lasty = y;
-        } else
-        {
-            stroke(0);
-            line( lastx, lasty, x, y);
-            lastx = x;
-            lasty = y;
-        }
+        handleMouse();
     } else
     {
         lastx = -1;
         lasty = -1;
+    }
+}
+
+function handleMouse()
+{
+    x = mouseX;
+    y = mouseY;
+    if( lastx == -1)
+    {
+        lastx = x;
+        lasty = y;
+    } else
+    {
+        if( brushOption.value() == "pencil")
+        {
+            stroke( black);
+            strokeWeight( brushSize.value());
+            line( lastx, lasty, x, y);
+        } else if( brushOption.value() == "eraser")
+        {
+            stroke( backgroundColor);
+            strokeWeight( brushSize.value() * 2);
+            line( lastx, lasty, x, y);
+        }
+        lastx = x;
+        lasty = y;
     }
 }
