f1c3819e61ae95e652ae73b444171db92f062937
Kiraub <kiraub.1997@gmail.com>
2017-12-12 00:53:08 +0100

    Updated to 12.12.2017, also changed the file structure a bit Removed the menutest as it is fully implemented in the main scattervis.html

--- scvis/scattervis.js
+++ scvis/scattervis.js
@@ -2,15 +2,15 @@
 
 const MAP_W = 100;
 const MAP_H = 60;
-const DATA_NAMES = ["Depth",                    
-                    "Salt %",                    
-                    "Oxygen %",                    
-                   ];                       
+const DATA_NAMES = ["Depth",
+                    "Salt %",
+                    "Oxygen %",
+                   ];
 const DATA_LEN = DATA_NAMES.length;
 
 let canvas = document.getElementById("scatterDataCanvas");
 let context = canvas.getContext("2d");
-let w, h;  
+let w, h;
 
 let scatterDataList = initialScatterData();
 let selectedDataPoint = null;
@@ -24,9 +24,9 @@ initTable();
 initSelector();
 drawScatterData();
 
-function doResize() {        
-    initCanvas();    
-    
+function doResize() {
+    initCanvas();
+
     voronoiDiagram = null;
     drawScatterData();
 }
@@ -36,11 +36,11 @@ function drawScatterData() {
     context.rect(0, 0, w, h);
     context.fillStyle = "#ffffff";
     context.fill();
-        
-    let data = normalizedData();        
-    
+
+    let data = normalizedData();
+
     getLayerOrder();
-    console.log(layerOrder);
+    //console.log(layerOrder);
 
     let checkShepard = document.getElementById("checkShepard");
     let checkVoronoi = document.getElementById("checkVoronoi");
@@ -62,8 +62,12 @@ function drawScatterData() {
 }
 
 function initCanvas() {
+    /* Width and Height have to be reset to avoid the canvas growing indefinitely */
+    canvas.height = 0;
+    canvas.width  = 0;
+
+    h = canvas.height = canvas.offsetHeight;
     w = canvas.width  = canvas.offsetWidth;
-    h = canvas.height = canvas.offsetHeight;       
 }
 
 function initTable() {
@@ -71,7 +75,7 @@ function initTable() {
     for (let i = 0; i < DATA_LEN; ++i) {
         let row = table.insertRow(i + 1);
         row.insertCell(0).innerHTML = DATA_NAMES[i];
-        
+
         let input = document.createElement("input");
         input.id = "input" + i;        
         input.type = "number";        
@@ -97,15 +101,14 @@ function initSelector() {
 }
 
 function onDataChange(dataIndex, input) {
-    
     if (selectedDataPoint == null) {
         console.log("this should not happen");
         return;
     }
-    let value = parseFloat(input.value);    
+    let value = parseFloat(input.value);
     if (value)
-        selectedDataPoint.data[selectedDataIndex] = value;        
-    
+        selectedDataPoint.data[selectedDataIndex] = value;
+
     voronoiDiagram = null;
     drawScatterData();
 }
@@ -113,7 +116,7 @@ function onDataChange(dataIndex, input) {
 function onDataSetChange() {
     let selector = document.getElementById("data-selector");
     selectedDataIndex = selector.selectedIndex;
-    
+
     voronoiDiagram = null;
     drawScatterData();
 }
@@ -122,16 +125,53 @@ function onClick(event) {
     let rect = canvas.getBoundingClientRect();
     let x = (event.clientX - rect.left) / w * MAP_W;
     let y = (event.clientY - rect.top) / h * MAP_H;
-    
+
     if (event.ctrlKey)
         addDataPoint(x, y);
-    else 
+    else if(event.shiftKey)
+        removeDataPoint(x, y);
+    else
         setSelectedDataPoint(getDataPointAt(x, y));
-    
+
      drawScatterData();
 }
 
-function getDataPointAt(x, y) {    
+// functions for dragging data points
+let draggedPoint = null;
+function onMouseDown(event) {
+    let rect = canvas.getBoundingClientRect();
+    let x = (event.clientX - rect.left) / w * MAP_W;
+    let y = (event.clientY - rect.top) / h * MAP_H;
+
+    draggedPoint = getDataPointAt(x, y);
+    setSelectedDataPoint(draggedPoint);
+    drawScatterData();
+}
+
+function onMouseMove(event) {
+    if (draggedPoint) {
+        let rect = canvas.getBoundingClientRect();
+        draggedPoint.x = (event.clientX - rect.left) / w * MAP_W;
+        draggedPoint.y = (event.clientY - rect.top) / h * MAP_H;
+
+        voronoiDiagram = null;
+        drawScatterData();
+    }
+}
+
+function onMouseUp(event) {
+    draggedPoint = null;
+}
+
+function onMouseOut(event) {
+    if (draggedPoint) {
+        draggedPoint = null;
+        setSelectedDataPoint(null);
+        drawScatterData();
+    }
+}
+
+function getDataPointAt(x, y) {
     for (let i = 0; i < scatterDataList.length; ++i) {
         let p = scatterDataList[i];
         let dx = p.x - x;
@@ -148,18 +188,18 @@ function scatterData(x, y, data) {
     return { x, y, data };
 }
 
-function initialScatterData() {       
+function initialScatterData() {
     let result = [
     scatterData(30, 10, [1.6, 0.037, 10.5]),
     scatterData(20, 50, [4.0, 0.070,  9.8]),
     scatterData(70, 20, [2.5, 0.055, 10.1]),
     scatterData(50, 30, [0.2, 0.041,  8.7]),
-    ];    
+    ];
     return result;
 }
 
-function toColor(value, alpha) {    
-    // TODO: fetch scale from color picker    
+function toColor(value, alpha) {
+    //TODO fetch scale from color picker    
     let scale = (value) => {
         let r = value * value;
         r = Math.floor(255 * r);
@@ -167,18 +207,19 @@ function toColor(value, alpha) {
         g = Math.floor(255 * g);
         let b = (1 - value) * (1 - value);
         b = Math.floor(255 * b);
-        
+
         return rgba(r, g, b, alpha);
     }
-    
+
     value = Math.max(Math.min(value, 1), 0);
     return scale(value);
 }
 
 function rgba(r, g, b, a) {
-    return { r, g, b, a, toString: function() {        
-        return "rgba(" + r + ", " + g + ", " + b + ", " + a + ")";
-    }
+    return { r, g, b, a,
+        toString: function() {
+            return "rgba(" + r + ", " + g + ", " + b + ", " + a + ")";
+        }
     };
 }
 
@@ -186,7 +227,7 @@ function normalizedData() {
     let result = [];
     let min = Number.MAX_VALUE;
     let max = Number.MIN_VALUE;
-    for (let i = 0; i < scatterDataList.length; ++i) {        
+    for (let i = 0; i < scatterDataList.length; ++i) {
         let x = scatterDataList[i].x * w / MAP_W;
         let y = scatterDataList[i].y * h / MAP_H;
         let value = scatterDataList[i].data[selectedDataIndex];
@@ -196,28 +237,28 @@ function normalizedData() {
         else if (value > max)
             max = value;
     };
-    let delta = max - min;    
+    let delta = max - min;
     for (let i = 0; i < result.length; ++i) 
-        result[i].value = (result[i].value - min) / delta;       
+        result[i].value = (result[i].value - min) / delta;
     return result;
 }
 
 function drawPointLayer(data) {
     for (let i = 0; i < data.length; ++i) {
-        let p = data[i];        
+        let p = data[i];
         // outline
         context.beginPath();
         context.arc(p.x, p.y, 6, 0, 2 * Math.PI);
-        context.lineWidth = 5;        
-        if (scatterDataList[i] == selectedDataPoint) 
-            context.strokeStyle = "#eeee00";            
+        context.lineWidth = 6;
+        if (scatterDataList[i] == selectedDataPoint)
+            context.strokeStyle = "#eeee00";
         else
-            context.strokeStyle = "#00000044";            
-        context.stroke();            
+            context.strokeStyle = "#00000044";
+        context.stroke();
         // inner circle
         context.beginPath();
         context.arc(p.x, p.y, 6, 0, 2 * Math.PI);
-        context.lineWidth = 3;        
+        context.lineWidth = 3;
         context.strokeStyle = toColor(p.value, 1.0);
         context.stroke();
     }
@@ -226,25 +267,25 @@ function drawPointLayer(data) {
 function drawVoronoiLayer(data, fill) {
     context.strokeStyle = "#000000";
     context.lineWidth = 1;
-    
-    if (voronoiDiagram == null) {        
+
+    if (voronoiDiagram == null) {
         let voronoi = d3.voronoi()
         voronoi.size([w, h])
         voronoi.x(point => point.x)
-        voronoi.y(point => point.y);        
+        voronoi.y(point => point.y);
         voronoiDiagram = voronoi(data);
     }        
-    let polygons = voronoiDiagram.polygons();    
+    let polygons = voronoiDiagram.polygons();
     for (let i = 0; i < polygons.length; ++i) {
-        let poly = polygons[i];        
+        let poly = polygons[i];
         context.beginPath();
         for (let j = 0; j < poly.length; ++j) 
             context.lineTo(poly[j][0], poly[j][1]);
         context.lineTo(poly[0][0], poly[0][1]);
         let value = poly.data.value;
         if (fill) {
-            context.fillStyle = toColor(value, 0.5);        
-            context.fill();        
+            context.fillStyle = toColor(value, 0.5);
+            context.fill();
         }
         context.stroke();
     }
@@ -252,27 +293,26 @@ function drawVoronoiLayer(data, fill) {
 
 function drawShepardLayer(data) {
     let image = context.createImageData(w, h);
-        
+
     for (let x = 0; x < w; ++x)
         for (let y = 0; y < h; ++y) {
             let totalDist = 0;
             let sum = 0;
-            
+
             for (let i = 0; i < data.length; ++i) {
                 let d = data[i];
                 let dist = (x - d.x) * (x - d.x) + (y - d.y) * (y - d.y);                
-                if (dist < 1) {            
+                if (dist < 1) {
                     totalDist = 1;
                     sum = d.value;
                     break;
-                }                
+                }
                 totalDist += 1 / dist;
                 sum += 1 / dist * d.value;
-            }            
-            let color = toColor(sum / totalDist, 1.0);               
+            }
+            let color = toColor(sum / totalDist, 1.0);
             setPixel(image, x, y, color);
         }
-    
     context.putImageData(image, 0, 0);
 }
 
@@ -292,26 +332,45 @@ function addDataPoint(x, y) {
     let data = new Array(DATA_LEN).fill(0); 
     // randomize
     for (let i = 0; i < DATA_LEN; ++i) {
-        let randomValue = 0.5 + Math.random() * 3;        
+        let randomValue = 0.5 + Math.random() * 3;
         data[i] = randomValue;
     }
     let p = scatterData(x, y, data);
     scatterDataList.push(p);
     setSelectedDataPoint(p);
-    
+
     voronoiDiagram = null;
+}
+
+function removeDataPoint(x, y) {
+    let p = getDataPointAt(x, y);
+    if(p) {
+        let i = 0;
+        for( i = 0; i < scatterDataList.length; i += 1) {
+            if( scatterDataList[i].data[0] == p.data[0] &&
+            scatterDataList[i].data[1] == p.data[1] ) {
+
+                scatterDataList.splice(i, 1);
+                setSelectedDataPoint(null);
+                voronoiDiagram = null;
+                break;
+            }
+        }
+    }
+    else
+        console.log("No data point at given coordinates.");
 }
 
 function setSelectedDataPoint(p) {
     selectedDataPoint = p;
-    for (let i = 0; i < DATA_LEN; ++i) {                 
-        let input = document.getElementById("input" + i);        
-        if (p) {                  
+    for (let i = 0; i < DATA_LEN; ++i) {
+        let input = document.getElementById("input" + i);
+        if (p) {
             input.disabled = false;
-            input.value = p.data[i].toFixed(2);  
+            input.value = p.data[i].toFixed(2);
         }
         else {
-            input.disabled = true;    
+            input.disabled = true;
             input.value = "";
         }
     }
